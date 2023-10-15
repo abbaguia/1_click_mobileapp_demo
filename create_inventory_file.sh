ba855800a666a3e3814bf091721b88eea93f045e
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2023-10-14 17:59:42 -0700

    Update create_inventory_file.sh

--- create_inventory_file.sh
+++ create_inventory_file.sh
@@ -1,20 +1,20 @@
 
 #!/usr/bin/bash
 
-echo -e "\n Checking to see if the inventory file /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml exisits"
+echo -e "\n Checking to see if the inventory file ~/workspace/step1-create-servers/1_click_mobileapp_demo/tf_inventory.yaml exisits"
 
-if [ -f "/var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml" ]; 
+if [ -f "~/workspace/step1-create-servers/1_click_mobileapp_demo/tf_inventory.yaml" ]; 
 then
-    echo -e "\n Found /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml, it will be deleted"
-    rm /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml
+    echo -e "\n Found ~/workspace/step1-create-servers/1_click_mobileapp_demo/tf_inventory.yaml, it will be deleted"
+    rm ~/workspace/step1-create-servers/1_click_mobileapp_demo/tf_inventory.yaml
     echo -e "\n Checking to verify that it is deleted ..... "
     pwd
     ls -ltr
 fi
-echo -e "\n Creating the //var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml now ...." 
-touch /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml
-echo -e "[servers]" > /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml
+echo -e "\n Creating the ~/workspace/step1-create-servers/1_click_mobileapp_demo/f_inventory.yaml now ...." 
+touch ~/workspace/step1-create-servers/1_click_mobileapp_demo/tf_inventory.yaml
+echo -e "[servers]" > ~/workspace/step1-create-servers/1_click_mobileapp_demo/tf_inventory.yaml
 pwd
 ls -ltr
 
-cat //var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml
+cat ~/workspace/step1-create-servers/1_click_mobileapp_demo/tf_inventory.yaml
