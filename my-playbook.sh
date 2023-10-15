da5961100beaeff683cd3723dbb9793498a24365
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2023-10-14 17:57:44 -0700

    Update my-playbook.sh
    reduced the size of directory names

--- my-playbook.sh
+++ my-playbook.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 export ANSIBLE_HOST_KEY_CHECKING=false
-ansible-playbook /var/lib/jenkins/1_click_mobileapp_demo/playbook.yaml  -i /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml -u opc --key-file ~/.ssh/cloudshellkey
+ansible-playbook ~/workspace/step1-create-servers/1_click_mobileapp_demo/playbook.yaml  -i ~/workspace/step1-create-servers/1_click_mobileapp_demo//tf_inventory.yaml -u opc --key-file ~/.ssh/cloudshellkey
