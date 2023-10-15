ad1c0026623cd9a9bee223a3711729d42feebaf3
Abbaguia <82067622+abbaguia@users.noreply.github.com>
2023-10-14 17:34:59 -0700

    Update create_inventory_file.sh
    Updating full jenkins path

--- create_inventory_file.sh
+++ create_inventory_file.sh
@@ -1,20 +1,20 @@
 
 #!/usr/bin/bash
 
-echo -e "\n Checking to see if the inventory file /home/mail2maham/tensorflow-demo-sanjose-src/ansible/tf_inventory.yaml exisits"
+echo -e "\n Checking to see if the inventory file /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml exisits"
 
-if [ -f "/home/mail2maham/tensorflow-demo-sanjose-src/ansible/tf_inventory.yaml" ]; 
+if [ -f "/var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml" ]; 
 then
-    echo -e "\n Found /home/mail2maham/tensorflow-demo-sanjose-src/ansible/tf_inventory.yaml, it will be deleted"
-    rm /home/mail2maham/tensorflow-demo-sanjose-src/ansible/tf_inventory.yaml
+    echo -e "\n Found /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml, it will be deleted"
+    rm /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml
     echo -e "\n Checking to verify that it is deleted ..... "
     pwd
     ls -ltr
 fi
-echo -e "\n Creating the /home/mail2maham/tensorflow-demo-sanjose-src/ansible/tf_inventory.yaml now ...." 
-touch /home/mail2maham/tensorflow-demo-sanjose-src/ansible/tf_inventory.yaml
-echo -e "[servers]" > /home/mail2maham/tensorflow-demo-sanjose-src/ansible/tf_inventory.yaml
+echo -e "\n Creating the //var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml now ...." 
+touch /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml
+echo -e "[servers]" > /var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml
 pwd
 ls -ltr
 
-cat /home/mail2maham/tensorflow-demo-sanjose-src/ansible/tf_inventory.yaml
+cat //var/lib/jenkins/1_click_mobileapp_demo/tf_inventory.yaml
